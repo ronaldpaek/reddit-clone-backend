c61bab3ac62b94d3e24b5528ac6bc7d51ec684d2
Ronald Paek <ronaldpaek@gmail.com>
2023-10-19 10:49:34 -0700

    update console.log

--- index.js
+++ index.js
@@ -36,6 +36,11 @@ const corsOptions = {
   credentials: true,
 };
 
+console.log(`DATABASE_URL: ${process.env.DATABASE_URL}`);
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+console.log(`JWT_SECRET: ${process.env.JWT_SECRET}`);
+console.log(`PORT: ${process.env.PORT}`);
+
 app.use(cors(corsOptions));
 
 cloudinary.config({
