0d97c561bd3b6cf6177489ce0c622e98916b2696
Ronald Paek <ronaldpaek@gmail.com>
2023-10-19 10:38:15 -0700

    update origin

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://joyful-pothos-3f79a4.netlify.app/",
     credentials: true,
   })
 );
